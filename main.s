fbda32463c9921748d1b1b6c0069acc31288b127
milcsu09 <petomilan2009@gmail.com>
2025-10-19 19:25:36 +0200

    Complicated functions

--- main.s
+++ main.s
@@ -81,49 +81,144 @@ square:
 	pop	rbp
 	ret
 
+	global factorial
+factorial:
+	push	rbp
+	mov	rbp, rsp
+	sub	rsp, 16
+	;
+	mov	qword [rbp-8], rdi
+	;
+	mov	r10, qword [rbp-8]
+	mov	r11, 1
+	sub	r10, r11
+	lea	r11, [rbp-16]
+	mov	qword [r11], r10
+	jmp	.L6
+.L5:
+	mov	r10, qword [rbp-8]
+	mov	r11, qword [rbp-16]
+	imul	r10, r11
+	lea	r11, [rbp-8]
+	mov	qword [r11], r10
+	mov	r10, qword [rbp-16]
+	mov	r11, 1
+	sub	r10, r11
+	lea	r11, [rbp-16]
+	mov	qword [r11], r10
+.L6:
+	mov	r10, qword [rbp-16]
+	mov	r11, 1
+	cmp	r10, r11
+	setg	r10b
+	test	r10b, r10b
+	jnz	.L5
+	mov	r10, qword [rbp-8]
+	mov	rax, r10
+	jmp	.L4
+	;
+.L4:
+	add	rsp, 16
+	pop	rbp
+	ret
+
+	global power
+power:
+	push	rbp
+	mov	rbp, rsp
+	sub	rsp, 32
+	;
+	mov	qword [rbp-8], rdi
+	;
+	mov	r10, qword [rbp-8]
+	lea	r11, [rbp-16]
+	mov	qword [r11], r10
+	mov	r10, 1
+	lea	r11, [rbp-24]
+	mov	qword [r11], r10
+	jmp	.L9
+.L8:
+	mov	r10, qword [rbp-8]
+	mov	r11, qword [rbp-16]
+	imul	r10, r11
+	lea	r11, [rbp-8]
+	mov	qword [r11], r10
+	mov	r10, qword [rbp-24]
+	mov	r11, 1
+	add	r10, r11
+	lea	r11, [rbp-24]
+	mov	qword [r11], r10
+.L9:
+	mov	r10, qword [rbp-24]
+	mov	r11, qword [rbp-16]
+	cmp	r10, r11
+	setl	r10b
+	test	r10b, r10b
+	jnz	.L8
+	mov	r10, qword [rbp-8]
+	mov	rax, r10
+	jmp	.L7
+	;
+.L7:
+	add	rsp, 32
+	pop	rbp
+	ret
+
 	global main
 main:
 	push	rbp
 	mov	rbp, rsp
-	sub	rsp, 48
+	sub	rsp, 64
 	;
 	;
 	lea	r10, [rel identity]
-	lea	r11, [rbp-32]
+	lea	r11, [rbp-48]
 	mov	r12, 0
 	imul	r12, 8
 	add	r11, r12
 	mov	qword [r11], r10
 	lea	r10, [rel successor]
-	lea	r11, [rbp-32]
+	lea	r11, [rbp-48]
 	mov	r12, 1
 	imul	r12, 8
 	add	r11, r12
 	mov	qword [r11], r10
 	lea	r10, [rel double]
-	lea	r11, [rbp-32]
+	lea	r11, [rbp-48]
 	mov	r12, 2
 	imul	r12, 8
 	add	r11, r12
 	mov	qword [r11], r10
 	lea	r10, [rel square]
-	lea	r11, [rbp-32]
+	lea	r11, [rbp-48]
 	mov	r12, 3
 	imul	r12, 8
 	add	r11, r12
 	mov	qword [r11], r10
+	lea	r10, [rel factorial]
+	lea	r11, [rbp-48]
+	mov	r12, 4
+	imul	r12, 8
+	add	r11, r12
+	mov	qword [r11], r10
+	lea	r10, [rel power]
+	lea	r11, [rbp-48]
+	mov	r12, 5
+	imul	r12, 8
+	add	r11, r12
+	mov	qword [r11], r10
 	mov	r10, 5
-	lea	r11, [rbp-40]
+	lea	r11, [rbp-56]
 	mov	qword [r11], r10
 	mov	r10, 0
-	lea	r11, [rbp-48]
+	lea	r11, [rbp-64]
 	mov	qword [r11], r10
-	jmp	.L6
-.L5:
-	mov	r10, qword [rbp-40]
+	jmp	.L12
+.L11:
+	mov	r10, qword [rbp-56]
 	mov	rdi, r10
-	lea	r10, [rbp-32]
-	mov	r11, qword [rbp-48]
+	lea	r10, [rbp-48]
+	mov	r11, qword [rbp-64]
 	imul	r11, 8
 	add	r10, r11
 	mov	r10, qword [r10]
@@ -143,24 +238,24 @@ main:
 	mov	r10, rax
 	mov	rdi, r10
 	call	printi
-	mov	r10, qword [rbp-48]
+	mov	r10, qword [rbp-64]
 	mov	r11, 1
 	add	r10, r11
-	lea	r11, [rbp-48]
+	lea	r11, [rbp-64]
 	mov	qword [r11], r10
-.L6:
-	mov	r10, qword [rbp-48]
-	mov	r11, 4
+.L12:
+	mov	r10, qword [rbp-64]
+	mov	r11, 6
 	cmp	r10, r11
 	setl	r10b
 	test	r10b, r10b
-	jnz	.L5
+	jnz	.L11
 	mov	r10, 0
 	mov	eax, r10d
-	jmp	.L4
+	jmp	.L10
 	;
-.L4:
-	add	rsp, 48
+.L10:
+	add	rsp, 64
 	pop	rbp
 	ret
 
