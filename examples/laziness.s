27cb1a56296e321bb89bc505e22dc1fcab3ac31e
milcsu09 <petomilan2009@gmail.com>
2025-11-25 11:02:19 +0100

    Example of lazy evaluation

---
+++ examples/laziness.s
@@ -0,0 +1,45 @@
+section .text
+	extern	printf
+
+printi:
+	mov	rsi, rdi
+	xor	eax, eax
+	mov	rdi, printi_s
+	jmp	printf
+
+	global	main
+main:
+	push	rbp
+	mov	rbp, rsp
+	sub	rsp, 16
+
+	mov	r10, 0
+	lea	r11, [rbp-8]
+	mov	qword [r11], r10
+	mov	r10, qword [rbp-8]
+	test	r10, r10
+	jz	.L1
+	mov	r10, qword [rbp-8]
+	mov	r10d, dword [r10]
+	movsx	r10, r10d
+	mov	r11, 10
+	cmp	r10, r11
+	sete	r10b
+	movzx	r10, r10b
+.L1:
+	mov	rdi, r10
+	call	printi
+	mov	r10, 0
+	mov	eax, r10d
+	jmp	.L0
+
+.L0:
+	add	rsp, 16
+	pop	rbp
+	ret
+
+section .rodata
+	printi_s: db "%ld", 10, 0
+
+section .note.GNU-stack
+
